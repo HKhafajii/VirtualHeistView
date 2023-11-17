d58da21437ac4deb8a0aaf246550d80ecebf0eb7
HKhafajii <alkhafajihassan@gmail.com>
2023-11-17 14:17:19 -0500

    asdasd

---
+++ VirtualHeistView/Collection.swift
@@ -0,0 +1,18 @@
+//
+//  Collection.swift
+//  VirtualHeistView
+//
+//  Created by Hassan Alkhafaji on 11/17/23.
+//
+
+import SwiftUI
+
+struct Collection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Collection()
+}
